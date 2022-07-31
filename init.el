aebbd8d4ee7c1dd7c0f7659cacc447d2a713cea2
Tell396 <vova21473@gmail.com>
2022-08-01 00:18:10 +1000

    Story #4. Update config.

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
+(setq package-selected-packages '(lsp-mode all-the-icons yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
   (mapc #'package-install package-selected-packages))
@@ -13,8 +13,10 @@
 ;; (package-refresh-contents)
 
 (add-to-list 'load-path "~/.emacs.d/evil")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/awesome-tab"))
 
 (require 'evil)
+(require 'awesome-tab)
 
 (unless (package-installed-p 'evil)
   (package-install 'evil))
@@ -27,37 +29,18 @@
 
 ;; Load theme
 (load-theme 'gruvbox t)
+(awesome-tab-mode t)
 
 ;; Ctrl+C, Ctrl+V copy, paste mode
 ;(global-set-key (kbd "C-c") 'kill-ring-save)
 ;(global-set-key (kbd "C-v") 'yank)
 
-
-;; Customize tabs
-(defvar my/tab-height 22)
-(defvar my/tab-left (powerline-wave-right 'tab-line nil my/tab-height))
-(defvar my/tab-right (powerline-wave-left nil 'tab-line my/tab-height))
-
-(defun my/tab-line-tab-name-buffer (buffer &optional _buffers)
-  (powerline-render (list my/tab-left
-                          (format "%s" (buffer-name buffer))
-                          my/tab-right)))
-(setq tab-line-tab-name-function #'my/tab-line-tab-name-buffer)
-
-;; tab color settings
-(set-face-attribute 'tab-line nil ;; background behind tabs
-      :background "gray40"
-      :foreground "gray60" :distant-foreground "gray50"
-      :height 1.0 :box nil)
-(set-face-attribute 'tab-line-tab nil ;; active tab in another window
-      :inherit 'tab-line
-      :foreground "gray70" :background "gray90" :box nil)
-(set-face-attribute 'tab-line-tab-current nil ;; active tab in current window
-      :background "#b34cb3" :foreground "white" :box nil)
-(set-face-attribute 'tab-line-tab-inactive nil ;; inactive tab
-      :background "gray60" :foreground "black" :box nil)
-(set-face-attribute 'tab-line-highlight nil ;; mouseover
-      :background "white" :foreground 'unspecified)
+;; Setting tabs
+(when (not (display-graphic-p))
+  (setq frame-background-mode 'dark))
+(setq awesome-tab-label-fixed-length 16)
+(setq awesome-tab-height 90)
+(setq awesome-tab-show-tab-index t)
 
 ;; Setting dashboard
 (setq dashboard-startup-banner "~/Изображения/Logos/dailyminimal/Olivia Black.jpeg")
