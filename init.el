bbd3b0b913642681f4708bd19b216a81151ca41b
Tell396 <vova21473@gmail.com>
2022-08-02 16:23:54 +1000

    Story #6. Adding use-package.

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(setq package-selected-packages '(all-the-icons auto-complete monokai-theme elcord lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
+;; (package-initialize)
+;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; (setq package-selected-packages '(all-the-icons auto-complete monokai-theme elcord lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
 
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
+;; (when (cl-find-if-not #'package-installed-p package-selected-packages)
+;;   (package-refresh-contents)
+;;   (mapc #'package-install package-selected-packages))
 
 ;; Set up package.el to work with MELPA
 ;; (require 'package)
@@ -13,14 +13,21 @@
 ;; (package-initialize)
 ;; (package-refresh-contents)
 
+
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/use-package")
 (add-to-list 'load-path "~/.emacs.d/evil")
+
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/awesome-tab"))
 (add-to-list 'load-path "~/.emacs.d/neotree")
+(add-to-list 'load-path "~/.emacs.d/powerline")
 
 (require 'evil)
 (require 'awesome-tab)
 (require 'elcord)
 (require 'lsp-mode)
+(require 'powerline)
+(require 'use-package)
 
 (unless (package-installed-p 'evil)
   (package-install 'evil))
@@ -28,10 +35,18 @@
 (evil-mode 1)
 (elcord-mode)
 (require 'neotree)
+(require 'powerline)
 
 ;; Company mode
 ;(setq company-idle-delay 0)
 ;(setq company-minimum-prefix-length 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; Keymap ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-set-key (kbd "C-x C-m") 'treemacs)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Setting packages ;;;;;;;;;;
@@ -42,16 +57,20 @@
 (tool-bar-mode -1)
 ;; (scroll-bar-mode -1)
 
+(setq-default tab-width 2)
+
 ;; Load theme
 (load-theme 'gruvbox t)
 (awesome-tab-mode t)
+
+(powerline-default-theme)
 
 ;; Ctrl+C, Ctrl+V copy, paste mode
 ;(global-set-key (kbd "C-c") 'kill-ring-save)
 ;(global-set-key (kbd "C-v") 'yank)
 
-;; Setting NeoTree
-(global-set-key [f8] 'neotree-toggle)
+;; Setting TreeMacs
+(global-set-key [f8] 'treemacs-toggle)
 
 ;; Setting tabs
 (when (not (display-graphic-p))
