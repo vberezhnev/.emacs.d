6aea1e804638c9a958a57d5c3af76b08d3c9bac5
Tell396 <vova21473@gmail.com>
2022-08-09 23:45:17 +1000

    Story #10. Update config.

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; (package-initialize)
 ;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(setq package-selected-packages '(all-the-icons ivy doom-themes auto-complete monokai-theme elcord lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
+;; (setq package-selected-packages '(all-the-icons ivy auto-complete monokai-theme elcord lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
 
 ;; (when (cl-find-if-not #'package-installed-p package-selected-packages)
 ;;   (package-refresh-contents)
@@ -19,46 +19,72 @@
 (add-to-list 'load-path "~/.emacs.d/evil")
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/awesome-tab"))
-(add-to-list 'load-path "~/.emacs.d/neotree")
+;(add-to-list 'load-path "~/.emacs.d/neotree")
 (add-to-list 'load-path "~/.emacs.d/powerline")
 
 (require 'evil)
 (require 'use-package)
 (require 'awesome-tab)
 (require 'elcord)
-(require 'lsp-mode)
 (require 'powerline)
 
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(require 'lsp-mode)
+
+(when (display-graphic-p)
+  (require 'all-the-icons))
+
+(use-package all-the-icons
+  :if (display-graphic-p))
 
 (evil-mode 1)
 (elcord-mode)
 (require 'neotree)
 (require 'powerline)
 
-(setq make-backup-files nil)
+(setq make-backup-files nil)          ; Delete #filename# files
+;(desktop-save-mode 1)                ; A global mode that automatically saves your Emacs session
+(display-line-numbers-mode)           ; Display numbers
+(setq display-line-numbers 'relative) ; Set relative numbers
+(xterm-mouse-mode 1)                  ; Mouse support for terminal
+
+;; Forces the messages to 0, and kills the *Messages* buffer - thus disabling it on startup.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Disabling the *scratch* buffer - thus disabling it on startup.
+(setq initial-major-mode (quote fundamental-mode))
+
+;; Disabled *Completions*
+(add-hook 'minibuffer-exit-hook 
+      '(lambda ()
+         (let ((buffer "*Completions*"))
+           (and (get-buffer buffer)
+            (kill-buffer buffer)))))
 
 ;; Company mode
 ;(setq company-idle-delay 0)
 ;(setq company-minimum-prefix-length 1)
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Keymap ;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Setting TreeMacs
-(global-set-key (kbd "C-x C-m") 'treemacs)
-(global-set-key [f8] 'treemacs)
-
 (global-auto-revert-mode t)
+
+(use-package neotree
+  :ensure t
+  :config (global-set-key [f8] 'neotree)
+          ; Every time when the neotree window is opened, let it find current file and jump to node.
+          (setq neo-smart-open t)
+          ; Do not autorefresh directory to show current file
+          (setq neo-autorefresh nil))
+
 
 (use-package treemacs
 	:config
 	(progn
     (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0))
-		      treemacs-show-hidden-files               -1
+		      treemacs-show-hidden-files               nil
 		)
 	
 	)
@@ -66,24 +92,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; Setting packages ;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
 
 (use-package projectile
   :demand t
@@ -103,13 +111,14 @@
 (tool-bar-mode -1)
 ;; (scroll-bar-mode -1)
 
-(setq-default tab-width 2)
+(setq-default tab-width 2) ; set default tab char's display width to 2 spaces
+(setq tab-width 2)         ; set current buffer's tab char's display width to 2 spaces
 
 ;; Restore previous session
 (desktop-save-mode 1)
 
 ;; Load theme
-(load-theme 'gruvbox t)
+;(load-theme 'gruvbox t)
 (awesome-tab-mode t)
 
 (powerline-default-theme)
@@ -137,10 +146,90 @@
                         (registers . 5)))
 (dashboard-setup-startup-hook)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;; My Funcs ;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; (defun my-insert-tab-char ()
+;   "insert a tab char. (ASCII 9, \t)"
+;   (interactive)
+;   (insert "\t")
+; 	)
+; 
+; (global-set-key (kbd "TAB") 'my-insert-tab-char)
+; (global-set-key (kbd "<tab>") 'my-insert-tab-char)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;; LSP ;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(lsp-treemacs-sync-mode 1)
+
+; Install company
+(use-package company
+  :ensure t
+  :config (add-hook 'prog-mode-hook 'company-mode)
+          (global-set-key (kbd "M-i") 'company-complete))
+(use-package company-anaconda 
+  :defer
+  :after company
+  :config (add-to-list 'company-backends 'company-anaconda))
+
+; Company language package for PHP
+(use-package company-php
+  :defer
+  :after company)
+
+; Just as an example, aso Ruby:
+(use-package robe ;; company-robe is a Ruby mode
+  :ensure t
+  :after company
+  :config (add-to-list 'company-backends 'company-robe)
+          (add-hook 'ruby-mode-hook 'robe-mode))
+
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+
+;; for completions
+(use-package company-lsp
+  :after lsp-mode
+  :config (push 'company-lsp company-backends))
+
+(use-package vue-mode
+  :mode "\\.vue\\'"
+  :config
+  (add-hook 'vue-mode-hook #'lsp))
+
+
+(add-hook 'prog-mode-hook 'linum-mode)
+(add-hook 'prog-mode-hook 'visual-line-mode)
+(add-hook 'prog-mode-hook 'show-paren-mode)
+(add-hook 'prog-mode-hook 'hs-minor-mode)
 
 ;;(custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -161,9 +250,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "5dbdb4a71a0e834318ae868143bb4329be492dd04bdf8b398fb103ba1b8c681a" default))
+	 '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" "a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "5dbdb4a71a0e834318ae868143bb4329be492dd04bdf8b398fb103ba1b8c681a" default))
  '(package-selected-packages
-	 '(## doom-themes all-the-icons ivy auto-complete monokai-theme elcord lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard)))
+	 '(## spacemacs-theme typescript-mode all-the-icons ivy auto-complete monokai-theme elcord lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck avy which-key helm-xref dap-mode gruvbox-theme json-mode dashboard))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
