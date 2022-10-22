3b4a74e1ea3f236c45700b7574f577feaa1a0bb9
Tell396 <vova21473@gmail.com>
2022-10-22 19:17:33 +1000

    Story #51. Fixing config.

--- init.el
+++ init.el
@@ -1,33 +1,27 @@
-(require 'package)
-;; (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-;;                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-refresh-contents)
 
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (unless package-archive-contents
-    (package-refresh-contents))
+;; Install use-package
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
   (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-;; Clean up all those temporary files
-(use-package no-littering)
 
 (org-babel-load-file
  (expand-file-name
   "README.org"
   user-emacs-directory))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(dired no-littering xclip web-mode use-package typescript-mode treemacs-evil treemacs-all-the-icons tree-sitter-langs reverse-im projectile pdf-tools parrot org-superstar multi-term magit-todos lsp-ui lsp-ivy json-mode helm-lsp gruvbox-theme go-mode fzf format-all flycheck-inline evil-multiedit evil-collection elfeed doom-themes doom-modeline diredfl dashboard dap-mode company-statistics company-quickhelp company-box company-anaconda blamer apheleia)))
+	 '(indent-guide no-littering xclip web-mode use-package typescript-mode treemacs-evil treemacs-all-the-icons tree-sitter-langs reverse-im projectile pdf-tools parrot org-superstar multi-term magit-todos lsp-ui lsp-ivy json-mode helm-lsp gruvbox-theme go-mode fzf format-all flycheck-inline evil-multiedit evil-collection elfeed doom-themes doom-modeline diredfl dashboard dap-mode company-statistics company-quickhelp company-box company-anaconda blamer apheleia)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
