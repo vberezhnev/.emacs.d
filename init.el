a96f996713082346a48804ad2536e8acf70e0453
Tell396 <vova21473@gmail.com>
2022-11-12 21:35:41 +1000

    Story #57. Automation.

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blamer-face ((t :foreground "#E46876" :height 140 :italic t)))
+ '(blamer-face ((t :foreground "#E46876" :height 140 :italic t)) t)
  '(mode-line ((t (:family "Iosevka Aile" :height 1.0))))
  '(mode-line-active ((t (:family "Iosevka Aile" :height 1.0))))
  '(mode-line-inactive ((t (:family "Iosevka Aile" :height 0.95)))))
