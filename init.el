52fedde23a227230ddcfde6c351f3ab5435f190a
Berezhnev Vladimir <vova21473@gmail.com>
2023-03-19 20:57:29 +1000

    Story #70. Org noter and some functions were added

--- init.el
+++ init.el
@@ -136,7 +136,7 @@
  ;; If there is more than one, they won't work right.
  '(before-save-hook '(parrot-start-animation format-all-buffer))
  '(custom-safe-themes
-	 '("83f4659ff482028561aed742320eaa491b1416c7c43181647f0c8ef256610dca" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "" default))
+	 '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "83f4659ff482028561aed742320eaa491b1416c7c43181647f0c8ef256610dca" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "" default))
  '(doom-modeline-bar-width 6)
  '(doom-modeline-buffer-file-name-style 'auto)
  '(doom-modeline-buffer-name nil)
@@ -148,6 +148,8 @@
  '(global-wakatime-mode t)
  '(helm-minibuffer-history-key "M-p")
  '(magit-todos-keywords (list "TODO" "FIXME" "HACK"))
+ '(org-agenda-files
+	 '("/home/chopin/Org/agenda/calendar.org" "/home/chopin/Org/agenda/agenda.org" "/home/chopin/Org/agenda/cal_inbox.org" "/home/chopin/Org/2Brain/2023-03-10-05:45:45-ооп_это.org" "/home/chopin/Org/2Brain/2023-02-04-16:19:33-школьные_предметы.org" "/home/chopin/Org/2Brain/2023-02-27-23:38:55-7_советов_по_очистке_кода_react.org" "/home/chopin/Org/2Brain/2023-03-05-20:41:26-latin_gender.org" "/home/chopin/Org/2Brain/2023-02-13-19:00:50-rust.org" "/home/chopin/Org/2Brain/20230202193313-bibliotheca_alexandrina.org"))
  '(org-hugo-base-dir ~/Templates/Blog)
  '(org-safe-remote-resources
 	 '("\\`https://cs7\\.pikabu\\.ru/post_img/2018/12/17/7/1545041477174040899\\.jpg\\'" "\\`https://cs11\\.pikabu\\.ru/post_img/2018/12/17/7/1545041626180870887\\.jpg\\'" "\\`https://upload\\.wikimedia\\.org/wikipedia/commons/thumb/0/00/Giovanni_Stanchi%2C_Watermelons%2C_Peaches%2C_Pears%2C_and_Other_Fruit_in_a_Landscape\\.jpg/2560px-Giovanni_Stanchi%2C_Watermelons%2C_Peaches%2C_Pears%2C_and_Other_Fruit_in_a_Landscape\\.jpg\\'" "\\`https://fniessen\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'" "\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)"))
@@ -160,8 +162,13 @@
  '(telega-root-fill-column 232)
  '(telega-sticker-animated-play nil)
  '(telega-translate-to-language-by-default "en")
- '(warning-suppress-log-types '(((org-roam)) ((org-roam)) (comp)))
- '(warning-suppress-types '(((org-roam)) (comp))))
+ '(warning-suppress-log-types
+	 '((use-package)
+		 (use-package)
+		 (org-element-cache)
+		 ((org-roam))
+		 (comp)))
+ '(warning-suppress-types '((use-package) (org-element-cache) ((org-roam)) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
