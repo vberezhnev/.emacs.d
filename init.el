3140938c2d9b2a7b262e4d5c76d6a4e9cb713eac
Berezhnev Vladimir <vova21473@gmail.com>
2023-05-18 00:21:59 +1000

    Story #75. Update org-mode.

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-(require 'package)
-																				; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; (setq package-archives
 ;;       '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
 ;; 	("ORG"		. "https://orgmode.org/elpa/")
@@ -18,88 +16,30 @@
                                    ("nongnu" . 1)))
 (package-initialize)
 
-;; (setq debug-on-error t)
+;;(package-refresh-contents)
 
-;; ;;(package-refresh-contents)
+;; (require 'package)
+;; ;; MELPA 20
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
+;; (setcdr (assoc "melpa" package-archive-priorities) 20)
 
-;; (save-excursion
-;;   (let ((tem eval-buffer-list))
-;;     (while (and tem
-;;                 (re-search-forward "^  eval-\\(buffer\\|region\\)(" nil t))
-;;       (beginning-of-line)
-;;       (insert (apply 'format "Error at line %d, column %d (point %d) in %s\n"
-;;                      (with-current-buffer (car tem)
-;;                        (list (line-number-at-pos (point))
-;;                              (current-column)
-;;                              (point)
-;;                              (buffer-name)))))
-;;       (pop tem))))
+;; ;; ELPA 15
+;; (add-to-list 'package-archives
+;;              '("elpa" . "https://elpa.gnu.org/packages/") t)
+;; (setcdr (assoc "elpa" package-archive-priorities) 15)
 
-;; (with-eval-after-load 'debug
-;;   (defun debugger-setup-buffer (debugger-args)
-;;     "Initialize the `*Backtrace*' buffer for entry to the debugger.
-;; That buffer should be current already."
-;;     (setq buffer-read-only nil)
-;;     (erase-buffer)
-;;     (set-buffer-multibyte t)        ;Why was it nil ?  -stef
-;;     (setq buffer-undo-list t)
-;;     (let ((standard-output (current-buffer))
-;;           (print-escape-newlines t)
-;;           (print-level 8)
-;;           (print-length 50))
-;;       (backtrace))
-;;     (goto-char (point-min))
-;;     (delete-region (point)
-;;                    (progn
-;;                      (search-forward "\n  debug(")
-;;                      (forward-line (if (eq (car debugger-args) 'debug)
-;;                                        2    ; Remove implement-debug-on-entry frame.
-;;                                      1))
-;;                      (point)))
-;;     (insert "Debugger entered")
-;;     ;; lambda is for debug-on-call when a function call is next.
-;;     ;; debug is for debug-on-entry function called.
-;;     (pcase (car debugger-args)
-;;       ((or `lambda `debug)
-;;        (insert "--entering a function:\n"))
-;;       ;; Exiting a function.
-;;       (`exit
-;;        (insert "--returning value: ")
-;;        (setq debugger-value (nth 1 debugger-args))
-;;        (prin1 debugger-value (current-buffer))
-;;        (insert ?\n)
-;;        (delete-char 1)
-;;        (insert ? )
-;;        (beginning-of-line))
-;;       ;; Debugger entered for an error.
-;;       (`error
-;;        (insert "--Lisp error: ")
-;;        (prin1 (nth 1 debugger-args) (current-buffer))
-;;        (insert ?\n))
-;;       ;; debug-on-call, when the next thing is an eval.
-;;       (`t
-;;        (insert "--beginning evaluation of function call form:\n"))
-;;       ;; User calls debug directly.
-;;       (_
-;;        (insert ": ")
-;;        (prin1 (if (eq (car debugger-args) 'nil)
-;;                   (cdr debugger-args) debugger-args)
-;;               (current-buffer))
-;;        (insert ?\n)))
-;;     ;; After any frame that uses eval-buffer,
-;;     ;; insert a line that states the buffer position it's reading at.
-;;     (save-excursion
-;;       (let ((tem eval-buffer-list))
-;;         (while (and tem
-;;                     (re-search-forward "^  eval-\\(buffer\\|region\\)(" nil t))
-;;           (beginning-of-line)
-;;           (insert (format "Error at line %d in %s: "
-;;                           (with-current-buffer (car tem)
-;;                             (line-number-at-pos (point)))
-;;                           (with-current-buffer (car tem)
-;;                             (buffer-name))))
-;;           (pop tem))))
-;;     (debugger-make-xrefs)))
+;; ;; NonGNU 10
+;; (add-to-list 'package-archives
+;;              '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+;; (setcdr (assoc "nongnu" package-archive-priorities) 10)
+
+;; ;; Org 30
+;; (add-to-list 'package-archives
+;;              '("org" . "https://orgmode.org/elpa/") t)
+;; (setcdr (assoc "org" package-archive-priorities) 30)
+
+;; (package-initialize)
 
 ;; Install use-package
 (when (not (package-installed-p 'use-package))
@@ -127,16 +67,12 @@
   "README.org"
   user-emacs-directory))
 
-;; (load "~/.emacs.d/readme")
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(before-save-hook '(parrot-start-animation format-all-buffer))
- '(custom-safe-themes
-	 '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "83f4659ff482028561aed742320eaa491b1416c7c43181647f0c8ef256610dca" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "19a2c0b92a6aa1580f1be2deb7b8a8e3a4857b6c6ccf522d00547878837267e7" "3e374bb5eb46eb59dbd92578cae54b16de138bc2e8a31a2451bf6fdb0f3fd81b" "" default))
  '(doom-modeline-bar-width 6)
  '(doom-modeline-buffer-file-name-style 'auto)
  '(doom-modeline-buffer-name nil)
