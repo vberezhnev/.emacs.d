127b30fa212eeecc4b96c5cf4c7da60cb45f4107
Berezhnev Vladimir <vova21473@gmail.com>
2023-06-10 18:17:15 +1000

    Story #76. Bugfix & update packages

--- init.el
+++ init.el
@@ -1,45 +1,18 @@
-;; (setq package-archives
-;;       '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
-;; 	("ORG"		. "https://orgmode.org/elpa/")
-;;         ("MELPA Stable" . "https://stable.melpa.org/packages/")
-;;         ("MELPA"        . "https://melpa.org/packages/"))
-;;       package-archive-priorities
-;;       '(("ORG"		. 20)
-;; 	("MELPA"        . 15)
-;; 	("MELPA Stable" . 10)
-;;         ("GNU ELPA"     . 5)))
-
-(add-to-list 'package-archives '("melpa"  . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-(setq package-archive-priorities '(("melpa"  . 3)
-                                   ("gnu"    . 2)
-                                   ("nongnu" . 1)))
-(package-initialize)
-
-;;(package-refresh-contents)
-
-;; (require 'package)
-;; ;; MELPA 20
-;; (add-to-list 'package-archives
-;;              '("melpa" . "https://melpa.org/packages/") t)
-;; (setcdr (assoc "melpa" package-archive-priorities) 20)
-
-;; ;; ELPA 15
-;; (add-to-list 'package-archives
-;;              '("elpa" . "https://elpa.gnu.org/packages/") t)
-;; (setcdr (assoc "elpa" package-archive-priorities) 15)
-
-;; ;; NonGNU 10
-;; (add-to-list 'package-archives
-;;              '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
-;; (setcdr (assoc "nongnu" package-archive-priorities) 10)
-
-;; ;; Org 30
-;; (add-to-list 'package-archives
-;;              '("org" . "https://orgmode.org/elpa/") t)
-;; (setcdr (assoc "org" package-archive-priorities) 30)
-
+;; (add-to-list 'package-archives '("melpa"  . "https://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+;; (setq package-archive-priorities '(("melpa"  . 3)
+;;                                    ("gnu"    . 2)
+;;                                    ("nongnu" . 1)))
 ;; (package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+             '("elpa" . "https://elpa.nongnu.org/nongnu/") t)
+(package-initialize)
 
 ;; Install use-package
 (when (not (package-installed-p 'use-package))
@@ -73,6 +46,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(before-save-hook '(parrot-start-animation format-all-buffer))
+ '(custom-safe-themes
+	 '("2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "fa49766f2acb82e0097e7512ae4a1d6f4af4d6f4655a48170d0a00bcb7183970" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "a589c43f8dd8761075a2d6b8d069fc985660e731ae26f6eddef7068fece8a414" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "512ce140ea9c1521ccaceaa0e73e2487e2d3826cc9d287275550b47c04072bc4" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
  '(doom-modeline-bar-width 6)
  '(doom-modeline-buffer-file-name-style 'auto)
  '(doom-modeline-buffer-name nil)
