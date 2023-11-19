05d2f5a474372f142d2cfff16e87ced102d0c03e
Berezhnev Vladimir <vova21473@gmail.com>
2023-11-19 10:27:02 +1000

    Story #127. Remove transparent functionality.

--- config/appereance-setting.el
+++ config/appereance-setting.el
@@ -5,24 +5,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;________________________________________________________________
-;;    Transparent Emacs
-;;________________________________________________________________
-(add-to-list 'default-frame-alist '(alpha . (100 . 100)))
-
-(defun toggle-transparency ()
-  (interactive)
-  (let ((alpha (frame-parameter nil 'alpha)))
-    (set-frame-parameter
-     nil 'alpha
-     (if (eql (cond ((numberp alpha) alpha)
-                    ((numberp (cdr alpha)) (cdr alpha))
-                    ;; Also handle undocumented (<active> <inactive>) form.
-                    ((numberp (cadr alpha)) (cadr alpha)))
-              100)
-         '(95 . 95) '(100 . 100)))))
-(global-set-key (kbd "C-c t") 'toggle-transparency)
-
-;;________________________________________________________________
 ;;    Setup fonts
 ;;________________________________________________________________
 (set-face-attribute 'default t
