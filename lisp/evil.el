229b1970606ee7776bf54333ce2a6088255eb7ec
Vladimir Berezhnev <vova21473@gmail.com>
2025-06-02 03:50:36 +1000

    `:recycle: refactor(evil): Reorganize Evil mode config`
    - Add detailed leader key bindings Initialize evil modules with
    - `use-package` Remove unused configurations Closes #123

--- lisp/evil.el
+++ lisp/evil.el
@@ -1,154 +1,235 @@
+;;; evil.el --- Evil mode configuration with leader key bindings
+
+;; Disable default Evil keybindings for some modes
 (setq evil-want-keybinding nil)
 
-;; (use-package xah-fly-keys
-;; 	:ensure nil)
-
-(use-package general
-	:ensure t
-	:config
-	(general-define-key
-	 :states '(normal visual insert emacs motion)
-	 "C-x l" 'evil-switch-to-windows-last-buffer
-	 "C-x SPC" 'hydra-main/body
-	 "C-x ;"  'my/smart-buffers-list
-	 "C-x , ;" 'consult-buffer))
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-ex-complete-emacs-commands nil
+        evil-vsplit-window-right t
+        evil-split-window-below t
+        evil-shift-round nil
+        evil-want-C-u-scroll t
+        evil-want-C-i-jump nil
+        evil-ex-set-initial-state 'normal
+        evil-shift-width 2)
+  :config
+  (evil-mode 1)
+  ;; Unbind arrow keys in motion, insert, and emacs states
+  (dolist (map '(evil-motion-state-map evil-insert-state-map evil-emacs-state-map))
+    (define-key (eval map) (kbd "<up>") nil)
+    (define-key (eval map) (kbd "<down>") nil)
+    (define-key (eval map) (kbd "<left>") nil)
+    (define-key (eval map) (kbd "<right>") nil))
+  ;; Unbind common Ctrl keys globally in normal state
+  (general-def :states 'normal
+    "C-x" nil
+    "C-c" nil))
 
 (use-package evil-leader
   :ensure t
-	:after evil
+  :after evil
   :config
-  (evil-leader/set-leader "SPC")
-	(evil-leader/set-key
-		"w" 'evil-window-vsplit)
+  (global-evil-leader-mode)
+  (evil-leader/set-leader "<SPC>"))
 
-	;; (evil-leader/set-key
-	;; "de" 'org-gtd-engage
-	;; "dr" 'org-gtd-engage-grouped-by-context
-	;; "dp" 'org-gtd-process-inbox
-	;; "c" 'org-gtd-organize)
-
-	(evil-leader/set-key
-		"z" '(org-agenda nil "z")
-		"qs" 'hq-shop
-		"qb" 'hq-buy-item
-		"qu" 'hq-use-item)
-
-	(evil-leader/set-key
-		;; "dc" 'org-gtd-capture
-		"dc" (lambda () (interactive) (org-gtd-capture nil "i"))
-		"de" 'org-gtd-engage
-		"dp" 'org-gtd-process-inbox
-		"dn" 'org-gtd-show-all-next
-		"ds" 'org-gtd-review-stuck-projects)
-
-  (global-evil-leader-mode))
-
-(use-package evil
-  :ensure t ;; install the evil package if not installed
-  :init ;; tweak evil's configuration before loading it
-  ;; (evil-select-search-module 'evil-search-module 'evil-search)
-  (setq evil-ex-complete-emacs-commands nil)
-  ;; (setq evil-vsplit-window-right t)
-  ;; (setq evil-split-window-below t)
-  ;;(setq evil-shift-round nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-
-  (setq evil-ex-set-initial-state 'normal)
-
-  :config ;; tweak evil after loading it
-  (evil-mode))
-
-;; (eval-after-load "evil-maps"
-;;   (dolist (map '(evil-motion-state-map
-;;                  evil-insert-state-map
-;;                  evil-emacs-state-map))
-;;     (define-key (eval map) (kbd "<up>") nil)
-;;     (define-key (eval map) (kbd "<down>") nil)
-;;     (define-key (eval map) (kbd "<left>") nil)
-;;     (define-key (eval map) (kbd "<right>") nil)))
-
-(use-package evil-terminal-cursor-changer
-	:ensure t
-	:config
-	(unless (display-graphic-p)
-    (require 'evil-terminal-cursor-changer)
-    (evil-terminal-cursor-changer-activate))
-	(setq evil-motion-state-cursor 'box)  ; █
-  (setq evil-visual-state-cursor 'box)  ; █
-  (setq evil-normal-state-cursor 'box)  ; █
-  (setq evil-insert-state-cursor 'bar)  ; ⎸
-  (setq evil-emacs-state-cursor  'hbar)) ; _
-
-(use-package evil-surround
+(use-package general
   :ensure t
+  :after evil-leader
   :config
-  (global-evil-surround-mode))
+	(general-define-key
+   :states '(normal visual insert emacs motion)
+   "C-x l" 'evil-switch-to-windows-last-buffer)
+	
+  ;; Define a leader key definer
+  (general-create-definer my-leader-def
+    :states '(normal visual motion)
+    :keymaps 'override
+    :prefix "<SPC>")
+  
+  ;; General keybindings under leader
+  (my-leader-def
+    "a" '(:ignore t :which-key "apps")
+    "aa" 'org-agenda
+    "cx" 'elfeed
+    
+    ;; Window management
+    ;; "w" '(:ignore t :which-key "windows")
+    ;; "wv" 'evil-window-vsplit
+    ;; "ws" 'evil-window-split
+    ;; "wd" 'evil-window-delete
+    ;; "wh" 'evil-window-left
+    ;; "wj" 'evil-window-down
+    ;; "wk" 'evil-window-up
+    ;; "wl" 'evil-window-right
+    
+    ;; File operations
+    "f" '(:ignore t :which-key "files")
+    "ff" 'helm-find-files
+    "fr" 'helm-recentf
+    "fs" 'save-buffer
+    
+    ;; Git/Magit
+    "g" '(:ignore t :which-key "git")
+    "gg" 'magit
+    "gc" 'magit-clone
 
-;; ;; @FIXME missing config
-;; (use-package evil-indent-textobject
-;;   :ensure t)
+		;; Eval
+    "e" '(:ignore t :which-key "eval")
+    "ee" 'eval-buffer
+    "er" 'eval-region
+		
+    ;; Org-mode
+    "o" '(:ignore t :which-key "org")
+    "oc" 'org-capture
+    "ol" 'org-store-link
+    "of" 'org-footnote-new
+    "op" 'my/org-pomodoro
+    "ot" 'org-timeblock
+    "og" 'org-gtd-capture
+    "oe" 'org-gtd-engage
+    "oi" 'org-gtd-process-inbox
+    "on" 'org-gtd-show-all-next
+    "os" 'org-gtd-review-stuck-projects
+    "ob" 'org-cite-insert
+    "odm" 'org-download-clipboard
+    "odo" 'org-download-image
+    "odi" 'org-cliplink
+    
+    ;; Org-roam
+    "n" '(:ignore t :which-key "notes")
+    "nl" 'org-roam-buffer-toggle
+    "nf" 'org-roam-node-find
+    "ni" 'org-roam-node-insert
+    "nc" 'org-roam-capture
+    "nt" 'org-roam-tag-add
+    "nr" 'org-roam-ref-add
+    "nj" 'org-roam-dailies-capture-today
+    
+    ;; AI/GPTel
+    "s" '(:ignore t :which-key "ai")
+    "ss" 'gptel
+    "sd" 'gptel-context-add
+    "sf" 'gptel-add-file
+    "sa" 'gptel-menu
+    "sr" 'gptel--regenerate
+    "se" 'gptel-rewrite
+    
+    ;; Misc
+    "m" '(:ignore t :which-key "misc")
+    "ms" 'scratch-buffer
+    "my" 'hydra-main/body
+    
+    ;; Toggle modes
+    "t" '(:ignore t :which-key "toggle")
+    "tt" 'treemacs
+    "td" 'treemacs-select-directory
+    "tb" 'treemacs-bookmark
+    
+    ;; Top-level bindings
+    "b" 'bluetooth-list-devices
+    "q" 'multi-vterm
+    "x" 'helm-M-x
+    "h" 'global-hide-mode-line-mode)
 
-(use-package evil-matchit
-  :ensure t
-  :config
-  (global-evil-matchit-mode))
+  ;; Mode-specific bindings
+  (general-def :states 'normal :keymaps 'elfeed-search-mode-map
+    "<return>" 'elfeed-search-show-entry
+    ;; "o" 'elfeed-search-browse-url
+    "r" 'elfeed-search-update--force
+    "u" 'elfeed-unjam
+    "af" 'elfeed-tube-fetch
+    "as" 'elfeed-tube-save)
 
-(use-package evil-commentary
-  :ensure t
-  :config
-  (evil-commentary-mode))
+  (general-def :states '(normal motion) :keymaps 'elfeed-show-mode-map
+    "o" 'elfeed-show-visit
+    ;; "j" 'elfeed-show-next
+    ;; "k" 'elfeed-show-prev
+    "f" 'elfeed-tube-fetch
+    "s" 'elfeed-tube-save)
 
-;; (use-package evil-set-option
-;;   :ensure t
-;;   :quelpa (evil-set-option
-;;            :fetcher github
-;;            :repo "FrostyX/evil-set-option"
-;;            :branch "main")
-;;   :config
-;;   (evil-set-option-mode))
+  (general-def :states 'normal :keymaps 'org-mode-map
+    "i" 'org-cite-insert
+    "dm" 'org-download-clipboard
+    "do" 'org-download-image
+    "di" 'org-cliplink)
+
+  (general-def :states 'normal :keymaps 'magit-mode-map
+    "SPC" nil) ; Unbind SPC to avoid leader key conflicts
+
+  (general-def :states 'normal :keymaps 'dired-mode-map
+    "." 'dired-omit-mode
+    "h" 'dired-up-directory
+    "l" 'dired-find-file
+    "s" 'save-buffer)
+
+  ;; Quick bindings in normal state (global)
+  (general-def :states 'normal
+    "s" 'save-buffer
+    "q" 'kill-current-buffer
+    "u" 'undo-tree-undo
+    "r" 'undo-tree-redo))
 
 (use-package evil-collection
   :ensure t
   :after evil
   :config
-	(setq evil-want-integration t)
+  (setq evil-want-integration t)
+  (evil-collection-init '(ibuffer bookmark vterm magit org-timeblock enlight xwidget-webkit sunrise helm))
   (evil-set-initial-state 'ibuffer-mode 'normal)
   (evil-set-initial-state 'bookmark-bmenu-mode 'normal)
   (evil-set-initial-state 'vterm-mode 'normal)
-	(evil-set-initial-state 'magit-mode 'emacs)
+  (evil-set-initial-state 'magit-mode 'emacs)
+  (evil-set-initial-state 'bluetooth-mode 'emacs)
   (evil-set-initial-state 'org-timeblock-mode 'emacs)
-  (evil-set-initial-state 'org-agenda-mode 'emacs)
+  ;; (evil-set-initial-state 'org-agenda-mode 'emacs)
   (evil-set-initial-state 'org-super-agenda-mode 'emacs)
-  (evil-set-initial-state 'calibredb-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'emacs)
+  ;; (evil-set-initial-state 'telega-mode-line-mode 'emacs)
+  ;; (evil-set-initial-state 'calibredb-mode 'normal)
   (evil-set-initial-state 'enlight-mode 'emacs)
-  (evil-set-initial-state 'org-timeblock-mode 'emacs)
   (evil-set-initial-state 'org-timeblock-list-mode 'emacs)
-  ;; (evil-set-initial-state 'dired-mode 'emacs)
   (evil-set-initial-state 'treemacs-mode 'emacs)
   (evil-set-initial-state 'xwidget-webkit-mode 'emacs)
-  (evil-set-initial-state 'sunrise-mode 'emacs)
-  (evil-collection-init))
+  (evil-set-initial-state 'sunrise-mode 'emacs))
 
-(use-package reverse-im
-  ;; :ensure t ; install `reverse-im' using package.el
-  :quelpa (reverse-im
-           :fetcher github
-           :repo "emacsmirror/reverse-im")
-  :demand t
-  ;;:after char-fold ; but only after `char-fold' is loaded
-  :custom
-  (reverse-im-cache-file (locate-user-emacs-file "reverse-im-cache.el"))
-  (reverse-im-char-fold t)
-  (reverse-im-read-char-advice-function #'reverse-im-read-char-include)
-  (reverse-im-input-methods '("ukrainian-computer"))
+(use-package evil-surround
+  :ensure t
   :config
-  (reverse-im-mode t)) ; turn the mode on
+  (global-evil-surround-mode 1))
+
+(use-package evil-matchit
+  :ensure t
+  :config
+  (global-evil-matchit-mode 1))
+
+(use-package evil-commentary
+  :ensure t
+  :config
+  (evil-commentary-mode 1))
+
+(use-package evil-terminal-cursor-changer
+  :ensure t
+  :config
+  (unless (display-graphic-p)
+    (evil-terminal-cursor-changer-activate))
+  (setq evil-motion-state-cursor 'box
+        evil-visual-state-cursor 'box
+        evil-normal-state-cursor 'box
+        evil-insert-state-cursor 'bar
+        evil-emacs-state-cursor 'hbar))
 
 (use-package undo-tree
   :ensure t
   :config
   (global-undo-tree-mode)
   (evil-set-undo-system 'undo-tree))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode 1)
+  (setq which-key-idle-delay 0.3))
 
 (provide 'evil)
