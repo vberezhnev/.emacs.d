122d319fed3c972f4ea20199fbb1d46d7f23ba3d
Vladimir Berezhnev <vova21473@gmail.com>
2025-06-02 03:48:56 +1000

    :gear: config: Update custom settings
    - Add new theme hash Add user photo size setting

--- custom.el
+++ custom.el
@@ -5,11 +5,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c"
+	 '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710"
 		 "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
 		 "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5"
 		 default))
- '(helm-minibuffer-history-key "M-p"))
+ '(helm-minibuffer-history-key "M-p")
+ '(telega-user-photo-size 16))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
