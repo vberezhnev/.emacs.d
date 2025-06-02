44e96875d6e3b7c9dd81fb4deb29d36a9ed26e49
Vladimir Berezhnev <vova21473@gmail.com>
2025-06-02 23:17:50 +1000

    :sparkles: feat(rss): Add reading mode and custom navigation
    - Add functions for centering text and hiding cursor in reading mode
    - Bind keys for custom view navigation in elfeed mode

--- lisp/rss.el
+++ lisp/rss.el
@@ -1,11 +1,71 @@
 (use-package nano-theme
 	:ensure t)
 
+(defun my/centre-visual-fill-on ()
+  ;; Centre the text in one column
+  (visual-line-mode 1)
+  (setq olivetti-body-width 100)
+  (olivetti-mode 1))
+
+(defun my/centre-visual-fill-off ()
+  (visual-line-mode 0)
+  (kill-local-variable 'olivetti-body-width)
+  (olivetti-mode 0))
+
+(defun my/kill-modeline ()
+  (setq-local mode-line-format nil))
+
+(defun my/restore-modeline ()
+    (kill-local-variable 'mode-line-format))
+
+(defun my/toggle-modeline ()
+  (interactive)
+  (if (null mode-line-format)
+      (my/restore-modeline)
+    (my/kill-modeline)))
+
+(defun my/hide-cursor-evil ()
+  (setq-local evil-default-cursor '(ignore))
+  (setq-local cursor-type nil))
+
+(defun my/show-cursor-evil ()
+  (kill-local-variable 'evil-default-cursor)
+  (kill-local-variable 'cursor-type))
+
+(defun my/toggle-cursor ()
+  (interactive)
+  (if (null cursor-type)
+      (my/show-cursor-evil)
+    (my/hide-cursor-evil)))
+
+(defun my/reading-mode-hook ()
+  (my/centre-visual-fill-on)
+  (my/hide-cursor-evil)
+  (my/kill-modeline))
+
+(defun my-view-down ()
+  (interactive)
+  (if (null cursor-type)
+      (scroll-up-line)
+    (next-line)))
+
+(defun my-view-up ()
+  (interactive)
+  (if (null cursor-type)
+      (scroll-down-line)
+    (previous-line)))
+
+
 (use-package elfeed
   :ensure t
   ;; :commands (elfeed)
   :bind ("C-c x" . elfeed) ; Global keybinding to launch elfeed
+	:hook
+	(elfeed-show-mode . my/reading-mode-hook)
   :config
+	(evil-define-key 'normal elfeed-show-mode-map (kbd "j") 'my-view-down)
+	(evil-define-key 'normal elfeed-show-mode-map (kbd "k") 'my-view-up)
+	
   (setq-default elfeed-search-filter "")
 	(setq elfeed-search-trailing-width 15)
 	(defun elfeed-search-format-date (date) "")
