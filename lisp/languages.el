6ea540bd7cadf8a5432f379bf4dea3f0b8938944
Vladimir Berezhnev <vova21473@gmail.com>
2025-06-06 05:21:04 +1000

    :recycle: refactor(languages): Comment out unused packages
    - Comment out several use-package blocks Update flycheck hook usage
    - Switch from eglot to lsp-bridge Update key bindings for lsp-bridge

--- lisp/languages.el
+++ lisp/languages.el
@@ -3,19 +3,19 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;; COMPANY ;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package company
-  :ensure t
-  :config
-  (setq company-idle-delay 0.1          
-        company-minimum-prefix-length 1 
-        company-dabbrev-ignore-case nil
-        company-dabbrev-downcase nil
-        company-selection-wrap-around t 
-        company-transformers '(company-sort-by-occurrence)) 
-  (add-hook 'after-init-hook 'global-company-mode)
-  (define-key company-active-map (kbd "TAB") 'company-complete-selection)
-  (define-key company-active-map (kbd "<return>") 'company-complete-selection)
-  (setq company-frontends '(company-pseudo-tooltip-frontend))) ; Легкий фронтенд
+;; (use-package company
+;;   :ensure t
+;;   :config
+;;   (setq company-idle-delay 0.1          
+;;         company-minimum-prefix-length 1 
+;;         company-dabbrev-ignore-case nil
+;;         company-dabbrev-downcase nil
+;;         company-selection-wrap-around t 
+;;         company-transformers '(company-sort-by-occurrence)) 
+;;   (add-hook 'after-init-hook 'global-company-mode)
+;;   (define-key company-active-map (kbd "TAB") 'company-complete-selection)
+;;   (define-key company-active-map (kbd "<return>") 'company-complete-selection)
+;;   (setq company-frontends '(company-pseudo-tooltip-frontend))) ; Легкий фронтенд
 
 ;;;;;;;;;;;;;;;;;;;;;;; COMPANY-BOX ;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -31,14 +31,14 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;; COMPANY-ORG-BLOCK ;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package company-org-block
-  :ensure t
-  :defer t
-  :custom
-  (company-org-block-edit-style 'auto)
-  :hook ((org-mode . (lambda ()
-                       (setq-local company-backends '(company-org-block))
-                       (company-mode +1)))))
+;; (use-package company-org-block
+  ;; :ensure t
+  ;; :defer t
+  ;; :custom
+  ;; (company-org-block-edit-style 'auto)
+  ;; :hook ((org-mode . (lambda ()
+  ;;                      (setq-local company-backends '(company-org-block))
+  ;;                      (company-mode +1)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;; DIFF-HL ;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -51,7 +51,7 @@
   :ensure t
   :demand t
   :config
-  (add-hook 'eglot-managed-mode-hook #'flycheck-mode)
+  (add-hook 'lsp-bridge-mode-hook #'flycheck-mode)
   (setq flycheck-check-syntax-automatically '(save idle-change))
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
@@ -61,11 +61,11 @@
 	:config
   (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
 
-(use-package flycheck-rust
-	:ensure t
-	:after flycheck
-	:config
-	(add-hook 'rustic-mode-hook #'flycheck-rust-setup))
+;; (use-package flycheck-rust
+;; 	:ensure t
+;; 	:after flycheck
+;; 	:config
+;; 	(add-hook 'rustic-mode-hook #'flycheck-rust-setup))
 
 ;;;;;;;;;;;;;;;;;;;;;;; YASNIPPET ;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -99,32 +99,31 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;; RUSTIC ;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package rustic
-  :ensure t
-  :after (eglot flycheck)
-  :defer t
-  :bind (:map rustic-mode-map
-              ("M-j" . lsp-ui-imenu)
-              ("M-?" . lsp-find-references)
-              ("C-c C-c l" . flycheck-list-errors)
-              ("C-c C-c a" . lsp-execute-code-action)
-              ("C-c C-c r" . lsp-rename)
-              ("C-c C-c q" . lsp-workspace-restart)
-              ("C-c C-c Q" . lsp-workspace-shutdown)
-              ("C-c C-c t" . rustic-cargo-test)
-              ("C-c C-c r" . rustic-cargo-run)
-              ("C-c C-c s" . lsp-rust-analyzer-status))
-  :mode ("\\.rs$" . rustic-mode)
-  :custom
-  (rustic-lsp-client 'eglot)
-  (rustic-lsp-server 'rust-analyzer)
-  :config
-  (add-hook 'rustic-mode-hook #'eglot-ensure)
-  (defun rk/rustic-mode-hook ()
-    (when buffer-file-name
-      (setq-local buffer-save-without-query t)))
-  (use-package rust-playground
-    :ensure t))
+;; (use-package rustic
+;;   :ensure t
+;;   :after (lsp-bridge flycheck)
+;;   :defer t
+;;   :bind (:map rustic-mode-map
+;;               ("M-j" . lsp-bridge-find-impl)
+;;               ("M-?" . lsp-bridge-find-references)
+;;               ("C-c C-c l" . flycheck-list-errors)
+;;               ("C-c C-c a" . lsp-bridge-code-action)
+;;               ("C-c C-c r" . lsp-bridge-rename)
+;;               ("C-c C-c q" . lsp-bridge-restart-process)
+;;               ("C-c C-c t" . rustic-cargo-test)
+;;               ("C-c C-c r" . rustic-cargo-run)
+;;               ("C-c C-c s" . lsp-bridge-diagnostic-list))
+;;   :mode ("\\.rs$" . rustic-mode)
+;;   :custom
+;;   (rustic-lsp-client nil) ;; Отключите eglot
+;;   :config
+;; 	(add-hook 'rustic-mode-hook #'lsp-bridge-mode)
+;;   (add-hook 'rustic-mode-hook (lambda () (company-mode -1)))
+;;   (defun rk/rustic-mode-hook ()
+;;     (when buffer-file-name
+;;       (setq-local buffer-save-without-query t)))
+;;   (use-package rust-playground
+;;     :ensure t))
 
 ;;;;;;;;;;;;;;;;;;;;;;; GO-MODE ;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -154,45 +153,70 @@
   :ensure t
   :after (company go-mode))
 
+;;;;;;;;;;;;;;;;;;;;;;; LSP-BRIDGE ;; ;;;;;;;;;;;;;;;;;;;;;
+
+(use-package lsp-bridge
+	:straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge"
+            :files (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+            :build (:not compile))
+	:init
+	(global-lsp-bridge-mode)
+  :config
+  ;; (global-lsp-bridge-mode)
+  (setq lsp-bridge-enable-log t
+        ;; lsp-bridge-rust-server "rust-analyzer"
+        lsp-bridge-enable-inlay-hint t
+        lsp-bridge-enable-diagnostics t
+        lsp-bridge-enable-signature-help t
+        ;; lsp-bridge-get-project-path-by-filepath
+        ;; (lambda (filepath) (locate-dominating-file filepath "Cargo.toml"))
+				)
+  :bind (:map lsp-bridge-mode-map
+              ("M-j" . lsp-bridge-find-def)
+              ("M-?" . lsp-bridge-find-references)
+              ("C-c C-c a" . lsp-bridge-code-action)
+              ("C-c C-c r" . lsp-bridge-rename)
+              ("C-c C-c l" . lsp-bridge-diagnostic-list)))
+
 ;;;;;;;;;;;;;;;;;;;;;;; EGLOT ;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package eglot
-  :ensure t
-  :commands (eglot eglot-ensure)
-  :config
-  (add-to-list 'eglot-server-programs
-               '((rustic-mode rust-mode) . ("rust-analyzer"))
-               ;; '((go-mode) . ("gopls"))
-               '((typescript-mode typescriptreact-mode) . ("typescript-language-server" "--stdio"))
-               ;; '((python-mode) . ("pylsp"))
-               ;; '((c-mode c++-mode) . ("clangd"))
-               '((js-mode) . ("typescript-language-server" "--stdio")))
-  (setq eglot-autoshutdown t
-        eglot-sync-connect 0
-        eglot-confirm-server-initiated-edits nil
-        eglot-extend-to-xref t
-        eglot-events-buffer-size 0
-        eglot-send-changes-idle-time 0.2)
-  (setq eldoc-idle-delay 0.0
-        eldoc-echo-area-use-multiline-p t
-        eldoc-documentation-strategy 'eldoc-documentation-compose)
-  (defun my/eglot-format-on-save ()
-    (when (eglot-managed-p)
-      (eglot-format-buffer)))
-  (add-hook 'before-save-hook #'my/eglot-format-on-save)
-  (add-hook 'eglot-managed-mode-hook
-            (lambda ()
-              (when (eglot--server-capable :inlayHintProvider)
-                (eglot-inlay-hints-mode))))
-  :bind (:map eglot-mode-map
-              ("C-c l f" . eglot-format-buffer)
-              ("C-c l r" . eglot-rename)
-              ("C-c l a" . eglot-code-actions)
-              ("C-c l d" . xref-find-definitions)
-              ("C-c l ?" . xref-find-references)
-              ("C-c l e" . flycheck-list-errors)
-              ("C-c l q" . eglot-shutdown)
-              ("M-j" . eglot-code-action-quickfix)))
+;; (use-package eglot
+;;   :ensure t
+;;   :commands (eglot eglot-ensure)
+;;   :config
+;;   (add-to-list 'eglot-server-programs
+;;                '((rustic-mode rust-mode) . ("rust-analyzer"))
+;;                ;; '((go-mode) . ("gopls"))
+;;                '((typescript-mode typescriptreact-mode) . ("typescript-language-server" "--stdio"))
+;;                ;; '((python-mode) . ("pylsp"))
+;;                ;; '((c-mode c++-mode) . ("clangd"))
+;;                '((js-mode) . ("typescript-language-server" "--stdio")))
+;;   (setq eglot-autoshutdown t
+;;         eglot-sync-connect 0
+;;         eglot-confirm-server-initiated-edits nil
+;;         eglot-extend-to-xref t
+;;         eglot-events-buffer-size 0
+;;         eglot-send-changes-idle-time 0.2)
+;;   (setq eldoc-idle-delay 0.0
+;;         eldoc-echo-area-use-multiline-p t
+;;         eldoc-documentation-strategy 'eldoc-documentation-compose)
+;;   (defun my/eglot-format-on-save ()
+;;     (when (eglot-managed-p)
+;;       (eglot-format-buffer)))
+;;   (add-hook 'before-save-hook #'my/eglot-format-on-save)
+;;   (add-hook 'eglot-managed-mode-hook
+;;             (lambda ()
+;;               (when (eglot--server-capable :inlayHintProvider)
+;;                 (eglot-inlay-hints-mode))))
+;;   :bind (:map eglot-mode-map
+;;               ("C-c l f" . eglot-format-buffer)
+;;               ("C-c l r" . eglot-rename)
+;;               ("C-c l a" . eglot-code-actions)
+;;               ("C-c l d" . xref-find-definitions)
+;;               ("C-c l ?" . xref-find-references)
+;;               ("C-c l e" . flycheck-list-errors)
+;;               ("C-c l q" . eglot-shutdown)
+;;               ("M-j" . eglot-code-action-quickfix)))
 
 ;;;;;;;;;;;;;;;;;;;;;;; HELM-LSP ;;;;;;;;;;;;;;;;;;;;;;;
 
